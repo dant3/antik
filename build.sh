a66c176321efa3d0065b9bb9a1a185d1ac2941a0
Vyacheslav Blinov <blinov.vyacheslav@gmail.com>
2012-10-12 01:46:08 +0400

    Don`t deploy on compile errors in build.sh script

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ android update project -p . -n antik -t 1 --subprojects
 
 # step2: build it and install it
 ant debug
-ant installd
+
+if [ $? == 0 ]
+then
+  ant installd
+else
+  echo "You had some compilation problems. Please fix them and try again."
+fi
